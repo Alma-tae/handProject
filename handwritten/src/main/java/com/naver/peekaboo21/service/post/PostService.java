87f2ccea1e64089fa0a8ede007c7a97e7a4a73f4
Alma-tae <taehuikim0430@gmail.com>
2021-06-12 12:01:09 +0900

    postOffice

---
+++ handwritten/src/main/java/com/naver/peekaboo21/service/post/PostService.java
@@ -0,0 +1,7 @@
+package com.naver.peekaboo21.service.post;
+
+import java.util.List;
+
+public interface PostService {
+	public List<String> xmlList(StringBuilder url);
+}
---
+++ handwritten/src/main/java/com/naver/peekaboo21/service/post/PostServiceImpl.java
@@ -0,0 +1,13 @@
+package com.naver.peekaboo21.service.post;
+
+import java.util.List;
+
+public class PostServiceImpl implements PostService {
+
+	@Override
+	public List<String> xmlList(StringBuilder url) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
