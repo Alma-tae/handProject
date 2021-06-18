237c7eec311d81f5d34650b34c0e911ead7ab351
Alma-tae <taehuikim0430@gmail.com>
2021-06-18 19:07:59 +0900

    Editing...

--- handwritten/src/main/java/com/naver/peekaboo21/controller/post/PostController.java
+++ handwritten/src/main/java/com/naver/peekaboo21/controller/post/PostController.java
@@ -1,34 +1,10 @@
 package com.naver.peekaboo21.controller.post;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.naver.peekaboo21.service.post.PostService;
 
 @Controller
 @RequestMapping("/post/*")
 public class PostController {
-	private String key = "API key";
-	
-	@Autowired
-	private PostService postService;
-	
-	@RequestMapping("list.do")
-	public String data(HttpSession session) {
-		session.setAttribute("category", "data");
-		
-		return "post/list.page";
-	}
 
-	@ResponseBody
-	@RequestMapping("postOffice")
-	public String postoffice() {
-		StringBuilder url = new StringBuilder("");
-		url.append("?ServiceKey=" + key);
-		return "";
-	}
 }
