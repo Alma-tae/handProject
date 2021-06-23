a021e6bac991482b24b967488d61b5d78c4b6f67
Alma-tae <taehuikim0430@gmail.com>
2021-06-23 17:31:28 +0900

    Editing...

--- handwritten/src/main/java/com/naver/peekaboo21/service/post/PostService.java
+++ handwritten/src/main/java/com/naver/peekaboo21/service/post/PostService.java
@@ -1,7 +1,5 @@
 package com.naver.peekaboo21.service.post;
 
-import java.util.List;
-
 public interface PostService {
-	public List<String> xmlList(StringBuilder url);
+
 }
--- handwritten/src/main/java/com/naver/peekaboo21/service/post/PostServiceImpl.java
+++ handwritten/src/main/java/com/naver/peekaboo21/service/post/PostServiceImpl.java
@@ -1,13 +1,6 @@
 package com.naver.peekaboo21.service.post;
 
-import java.util.List;
-
 public class PostServiceImpl implements PostService {
 
-	@Override
-	public List<String> xmlList(StringBuilder url) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
